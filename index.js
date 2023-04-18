fcde6447f516783c8afa7542f7c9628f1c21fc38
Mahmoud Safaan <you@example.com>
2023-04-19 00:22:12 +0200

    added branche

---
+++ index.js
@@ -0,0 +1,12 @@
+import express, { response } from 'express';
+const app = express();
+const students = [ "mohamed", "ahmed", "mahmoud", "safaan", "ayman", "elsayed", "hassan" ]
+const studentsfunction = (request, respose) => {
+let output = "<ul>";
+ for (let i = 0; i < students.length; i++) {
+ output += "<li>" + students[i] + "<li>" } 
+ output += '<ul>'
+respose.send(output) };
+app.get('/students', studentsfunction) 
+
+app.listen(5000);
